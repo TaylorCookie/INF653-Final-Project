89c985aa6b1d2f351b0de82b14f1abbf7da4b5bd
Taylor Cook <tecook2@mail.fhsu.edu>
2021-05-04 21:49:13 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -4,7 +4,7 @@ class Database {
     /*
     private static $dsn = 'mysql:host=y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=t1v10q62hhx7ktca';
     */
-    
+
     private static $db;
     private $conn;
 
@@ -14,17 +14,18 @@ class Database {
         $url = getenv('mysql://fg50z9bjvy50qzn5:v94dvu4svni96f58@y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/t1v10q62hhx7ktca');
         $dbparts = parse_url($url);
 
+        /*
         $hostname = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         $username = 'fg50z9bjvy50qzn5';
         $password = 'v94dvu4svni96f58';
         $database = 't1v10q62hhx7ktca';
-
-        /*
+        */
+        
         $hostname = $dbparts['host'];
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
         $database = ltrim($dbparts['path'],'/');
-        */
+        
 
         //set dsn variable
         $dsn = "mysql:host={$hostname};dbname={$database}";
