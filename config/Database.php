dd2b194fcf66408aed02d92519a28a4b5ffb7b00
Taylor Cook <tecook2@mail.fhsu.edu>
2021-05-04 21:45:22 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -3,15 +3,10 @@
 class Database {
     /*
     private static $dsn = 'mysql:host=y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=t1v10q62hhx7ktca';
-    private static $host = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    private static $username = 'fg50z9bjvy50qzn5';
-    private static $password = 'v94dvu4svni96f58';
-    private static $dbname = 't1v10q62hhx7ktca';
-    private static $db;
     */
-
+    
+    private static $db;
     private $conn;
-    //private static $db;
 
     //DB Connect
     public function connect() {
@@ -19,10 +14,17 @@ class Database {
         $url = getenv('mysql://fg50z9bjvy50qzn5:v94dvu4svni96f58@y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/t1v10q62hhx7ktca');
         $dbparts = parse_url($url);
 
+        $hostname = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        $username = 'fg50z9bjvy50qzn5';
+        $password = 'v94dvu4svni96f58';
+        $database = 't1v10q62hhx7ktca';
+
+        /*
         $hostname = $dbparts['host'];
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
         $database = ltrim($dbparts['path'],'/');
+        */
 
         //set dsn variable
         $dsn = "mysql:host={$hostname};dbname={$database}";
@@ -30,20 +32,20 @@ class Database {
         $this->conn = null;
 
         
-        //if (!isset(self::$db)) {
-        try {
-            $this->conn = new PDO($dsn, $username, $password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
-            //self::$db = new PDO($dsn, $username, $password);
-            
-            
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-            exit();
+        if (!isset(self::$db)) {
+            try {
+                $this->conn = new PDO($dsn, $username, $password);
+                self::$db = new PDO($dsn, $username, $password);
+                
+                
+            } catch (PDOException $e) {
+                $error_message = 'Database Error: ';
+                $error_message .= $e->getMessage();
+                echo $error_message;
+                exit();
+            }
         }
-        //}
-        return $this->conn;
+        return self::$db;
         
     }
 
