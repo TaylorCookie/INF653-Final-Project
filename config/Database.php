445740329e72d0573fe877bed908222e905d4146
Taylor Cook <tecook2@mail.fhsu.edu>
2021-05-04 20:16:06 -0500

    Updating JawsDB database

--- config/Database.php
+++ config/Database.php
@@ -1,22 +1,41 @@
 <?php
 
 class Database {
+    /*
     private static $dsn = 'mysql:host=y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=t1v10q62hhx7ktca';
     private static $host = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     private static $username = 'fg50z9bjvy50qzn5';
     private static $password = 'v94dvu4svni96f58';
     private static $dbname = 't1v10q62hhx7ktca';
     private static $db;
+    */
+
     private $conn;
+    private static $db;
 
     //DB Connect
     public function connect() {
+        //stuff from https://devcenter.heroku.com/articles/jawsdb#using-jawsdb-with-php
+        $url = getenv('mysql://fg50z9bjvy50qzn5:v94dvu4svni96f58@y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/t1v10q62hhx7ktca');
+        $dbparts = parse_url($url);
+
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+
+        //set dsn variable
+        $dsn = "mysql:host={$hostname};dbname={$database}";
+
         $this->conn = null;
 
         if (!isset(self::$db)) {
             try {
-                self::$db = new PDO(self::$dsn, self::$username, self::$password
-                );
+                $this->conn = new PDO($dsn, $username, $password);
+                /*
+                self::$db = new PDO(self::$dsn, self::$username, self::$password);
+                */
+                
             } catch (PDOException $e) {
                 $error_message = 'Database Error: ';
                 $error_message .= $e->getMessage();
