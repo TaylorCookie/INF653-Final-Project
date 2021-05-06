843101268433c08257a27800f0b90698d41adf75
Taylor Cook <tecook2@mail.fhsu.edu>
2021-05-06 18:32:46 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -14,17 +14,17 @@ class Database {
         $url = getenv('mysql://fg50z9bjvy50qzn5:v94dvu4svni96f58@y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/t1v10q62hhx7ktca');
         $dbparts = parse_url($url);
 
-        /*
         $hostname = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         $username = 'fg50z9bjvy50qzn5';
         $password = 'v94dvu4svni96f58';
         $database = 't1v10q62hhx7ktca';
-        */
         
+        /*
         $hostname = $dbparts['host'];
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
         $database = ltrim($dbparts['path'],'/');
+        */
         
         //set dsn variable
         $dsn = "mysql:host={$hostname};dbname={$database}";
